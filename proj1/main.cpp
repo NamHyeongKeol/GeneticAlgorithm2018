7c62d7ca0b21e96a587e2e840d5d45947e949ac0
royce-hpcnt <royce@hpcnt.com>
2018-04-02 23:00:40 +0900

    file input complete

--- proj1/main.cpp
+++ proj1/main.cpp
@@ -1,6 +1,24 @@
+#include <fstream>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main()
+{
+    string filePath = "../proj1_instances/unweighted_50.txt";
+
+    // read File
+    ifstream openFile(filePath.data());
+    if( openFile.is_open() ){
+        string line;
+        int n, k;
+        openFile >> n >> k;
+
+        while(getline(openFile, line)){
+            cout << line << endl;
+        }
+        openFile.close();
+    }
+
     return 0;
 }
